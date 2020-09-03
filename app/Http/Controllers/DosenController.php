cf3139b917d8e71d69b856ce56079938e31e91fa
fileham <fahmilham456@gmail.com>
2020-09-04 00:54:41 +0700

    Pengenalan Route, Membuat view, dan membuat controller

---
+++ app/Http/Controllers/DosenController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DosenController extends Controller
+{
+    //
+    public function index(){
+        return "Halo ini adalah method index, dalam controller DosenController. - www.malasngoding.com";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('halo', function () {
+	return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+});
+Route::get('blog', function () {
+	return view('blog');
+});
+Route::get('dosen','DosenController@index');

cafa650fd81aed980161c33053b60a770d972df4
fileham <fahmilham456@gmail.com>
2020-09-11 22:10:40 +0700

    mengatasi eror Route Pegawai(karena beda versi laravel)

--- belajar_crud/routes/web.php
+++ belajar_crud/routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about','PagesController@about');
+Route::get('/pegawai','App\Http\Controllers\PegawaiController@crud');

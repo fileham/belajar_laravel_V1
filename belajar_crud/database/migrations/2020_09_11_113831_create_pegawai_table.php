f892c552b21ac7f73e983d344982cdef4f516a08
fileham <fahmilham456@gmail.com>
2020-09-11 18:52:22 +0700

    membuat table pegawai dengan migration

---
+++ belajar_crud/database/migrations/2020_09_11_113831_create_pegawai_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePegawaiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->BigIncrements('pegawai_id');
+            $table->string('pegawai_nama',50);
+            $table->string('pegawai_jabatan',20);
+            $table->integer('pegawai_umur');
+            $table->text('pegawai_alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pegawai');
+    }
+}

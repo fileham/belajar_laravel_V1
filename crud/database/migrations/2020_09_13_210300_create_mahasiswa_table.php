96a5278fbf7dc649f03d4e096477cec5833cd991
fileham <fahmilham456@gmail.com>
2020-09-14 04:06:45 +0700

    menambah table mahasiswa dengan migrasi

---
+++ crud/database/migrations/2020_09_13_210300_create_mahasiswa_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->integer('nim');
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
